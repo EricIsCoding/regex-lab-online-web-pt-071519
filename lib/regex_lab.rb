02d5de29771b6906c1f98e212eb2ed45ce500c81
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-29 16:46:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,10 +2,9 @@ require "pry"
 
 def starts_with_a_vowel?(word)
   if word.match(/\b[aeiou]/i) != nil
-  answer = true
+  return true
   end
-  binding.pry
-  answer
+  false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
