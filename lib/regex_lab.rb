f578656963d54e0fa7299ac3edca1abcb769cce7
Eric J Wilhite <ericjwilhite@gmail.com>
2019-07-29 17:13:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ end
 def words_five_letters_long(text)
   text_array = text.split(" ")
   binding.pry
-  text_array.grep(/\w{5}$/)
+  text_array.grep(/\w{5}+$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
